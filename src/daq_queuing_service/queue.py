0b74fa3e81f29f2086309b9e0553a20a7210c552
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-04-08 16:44:33 +0100

    Improve structure and add more tests

--- src/daq_queuing_service/queue.py
+++ src/daq_queuing_service/queue.py
@@ -26,7 +26,7 @@ class TaskQueue:
             self.condition.notify_all()
             return task
 
-    async def complete_task(self, task_id: TaskID):
+    async def complete_task(self, task_id: TaskID, blueapi_info: str):
         async with self.condition:
             task = self._tasks.get(task_id)
             assert task is not None, f"Cannot find task with ID: {task_id}"
@@ -39,32 +39,73 @@ class TaskQueue:
             )
             self.queue.pop(0)
             task.update_status(Status.COMPLETED)
-            self.history.insert(0, task_id)
+            self.history.append(task_id)
             self.condition.notify_all()
 
     async def get_task_by_id(self, task_id: str) -> Task | None:
-        return self._tasks.get(task_id)
+        async with self.lock:
+            return self._tasks.get(task_id)
 
     async def get_task_by_position(self, position: int) -> Task | None:
-        return self._tasks[self.queue[position]] if position < self.length else None
+        async with self.lock:
+            return self._tasks[self.queue[position]] if position < self.length else None
+
+    async def get_queue(self) -> list[str]:
+        async with self.lock:
+            tasks = [self._tasks[task_id] for task_id in self.queue]
+        return [task.model_dump_json() for task in tasks]
+
+    async def get_history(self) -> list[str]:
+        async with self.lock:
+            tasks = [self._tasks[task_id] for task_id in self.history]
+        return [task.model_dump_json() for task in tasks]
+
+    async def get_tasks(self) -> list[str]:
+        async with self.lock:
+            tasks = [self._tasks[task_id] for task_id in self.history] + [
+                self._tasks[task_id] for task_id in self.queue
+            ]
+        return [task.model_dump_json() for task in tasks]
 
     async def add_tasks(self, tasks: list[Task], position: int | None = None) -> None:
         async with self.condition:
             self._verify_new_tasks(tasks, position)
+            if position is not None:
+                position = self._validate_position(position)
             self._add_tasks(tasks, position)
             self.condition.notify_all()
 
-    async def move_task(self, task_id: str, position: int):
+    async def move_task(self, task_id: TaskID, position: int):
         async with self.condition:
-            task = self._remove_tasks([task_id])
-            self._add_tasks(task, position)
+            position = self._validate_position(position)
+            task_ids = self._remove_tasks_from_queue([task_id])
+            self.queue[position:position] = task_ids
             self.condition.notify_all()
 
-    async def remove_tasks(self, task_ids: list[str]) -> list[Task]:
+    def _validate_position(self, position: int) -> int:
+        if position < 0:
+            raise ValueError(f"Position must be >= 0, got {position}")
+        if (
+            self.length
+            and position == 0
+            and self._tasks[self.queue[0]].status != Status.WAITING
+        ):
+            return 1
+        return position
+
+    async def remove_tasks(self, task_ids: list[TaskID]) -> list[Task]:
         async with self.condition:
-            tasks = self._remove_tasks(task_ids)
+            task_ids = self._remove_tasks_from_queue(task_ids)
+            tasks = self._remove_tasks_from_registry(task_ids)
             self.condition.notify_all()
             return tasks
+
+    async def clear_history(self):
+        async with self.condition:
+            for task_id in self.history:
+                self._tasks.pop(task_id)
+            self.history.clear()
+            self.condition.notify_all()
 
     def _add_tasks(self, tasks: list[Task], position: int | None) -> None:
         task_ids = [task.id for task in tasks]
@@ -75,7 +116,7 @@ class TaskQueue:
         for task in tasks:
             self._tasks[task.id] = task
 
-    def _remove_tasks(self, task_ids: list[str]) -> list[Task]:
+    def _remove_tasks_from_queue(self, task_ids: list[TaskID]) -> list[TaskID]:
         #  Only removes tasks in the queue (not history)
         def should_be_removed(task_id: TaskID):
             return (
@@ -83,15 +124,24 @@ class TaskQueue:
                 and self._tasks[task_id].status != Status.IN_PROGRESS
             )
 
-        removed = [
-            self._tasks[task_id] for task_id in task_ids if should_be_removed(task_id)
-        ]
-        removed_ids = [task.id for task in removed]
+        removed_ids = [task_id for task_id in task_ids if should_be_removed(task_id)]
         self.queue = [task_id for task_id in self.queue if task_id not in removed_ids]
+
+        return removed_ids
+
+    def _remove_tasks_from_registry(self, task_ids: list[TaskID]) -> list[Task]:
+        def should_be_removed(task_id: TaskID):
+            return (
+                task_id in self._tasks
+                and self._tasks[task_id].status != Status.IN_PROGRESS
+            )
+
+        removed_ids = [task_id for task_id in task_ids if should_be_removed(task_id)]
+        removed = [self._tasks[task_id] for task_id in removed_ids]
         self._tasks = {
             task_id: task
             for task_id, task in self._tasks.items()
-            if task_id not in removed_ids
+            if task.id not in removed_ids
         }
         return removed
 
--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,7 +1,7 @@
 import pytest
 
 from daq_queuing_service.queue import TaskQueue
-from daq_queuing_service.task import ExperimentDefinition, Task
+from daq_queuing_service.task import ExperimentDefinition, Status, Task
 
 pytest_plugins = ("pytest_asyncio",)
 
@@ -10,15 +10,25 @@ pytest_plugins = ("pytest_asyncio",)
 def tasks() -> list[Task]:
     return [
         Task(experiment_definition=ExperimentDefinition(sample_id=str(i)), id=str(i))
-        for i in range(3)
+        for i in range(5)
     ]
 
 
-async def test_queue_can_have_tasks_added(tasks: list[Task]):
+@pytest.fixture
+async def task_queue_in_progress(tasks: list[Task]):
     queue = TaskQueue()
     await queue.add_tasks(tasks)
-    assert queue.queue == ["0", "1", "2"]
-    assert list(queue.tasks.keys()) == ["0", "1", "2"]
+    first_task_id = queue.queue[0]
+    first_task = queue._tasks[first_task_id]  # type: ignore  # noqa
+    first_task.update_status(Status.IN_PROGRESS)
+    return queue
+
+
+async def test_add_tasks_works_as_expected(tasks: list[Task]):
+    queue = TaskQueue()
+    await queue.add_tasks(tasks)
+    assert queue.queue == ["0", "1", "2", "3", "4"]
+    assert set(queue._tasks.keys()) == {"0", "1", "2", "3", "4"}  # type: ignore  # noqa
 
 
 @pytest.mark.parametrize(
@@ -31,12 +41,9 @@ async def test_queue_can_have_tasks_added(tasks: list[Task]):
         [0, 9, [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]],
     ],
 )
-async def test_queue_can_move_multiple_tasks_at_once(
+async def test_move_task_works_as_expected(
     task_to_move: int, new_position: int, expected_order: list[int]
 ):
-    a = 5 + 1
-    b = a
-    a = b
     queue = TaskQueue()
     tasks = [
         Task(experiment_definition=ExperimentDefinition(sample_id=str(i)), id=str(i))
@@ -47,3 +54,47 @@ async def test_queue_can_move_multiple_tasks_at_once(
     await queue.move_task(task, new_position)
     result_order = [int(task_id) for task_id in queue.queue]
     assert result_order == expected_order
+
+
+async def test_move_task_to_position_0_moves_to_position_1_if_first_task_in_progress(
+    task_queue_in_progress: TaskQueue,
+):
+    task = await task_queue_in_progress.get_task_by_position(0)
+    assert task and task.status == Status.IN_PROGRESS
+
+    await task_queue_in_progress.move_task("4", 0)
+    assert task_queue_in_progress.queue == ["0", "4", "1", "2", "3"]
+
+
+async def test_move_task_does_not_move_task_that_is_in_progress(
+    task_queue_in_progress: TaskQueue,
+):
+    task = await task_queue_in_progress.get_task_by_position(0)
+    assert task and task.status == Status.IN_PROGRESS
+
+    await task_queue_in_progress.move_task("0", 3)
+    assert task_queue_in_progress.queue == ["0", "1", "2", "3", "4"]
+
+
+async def test_remove_tasks_works_as_expected(tasks: list[Task]):
+    queue = TaskQueue()
+    await queue.add_tasks(tasks)
+    await queue.remove_tasks(["4", "2"])
+    assert queue.queue == ["0", "1", "3"]
+
+
+async def test_remove_tasks_does_not_remove_task_that_is_in_progress(
+    task_queue_in_progress: TaskQueue,
+):
+    task = await task_queue_in_progress.get_task_by_position(0)
+    assert task and task.status == Status.IN_PROGRESS
+
+    await task_queue_in_progress.remove_tasks(["0", "2"])
+    assert task_queue_in_progress.queue == ["0", "1", "3", "4"]
+
+
+async def test_remove_tasks_does_not_error_if_wrong_task_id_used(tasks: list[Task]):
+    queue = TaskQueue()
+    await queue.add_tasks(tasks)
+    await queue.remove_tasks(["4", "2", "10"])
+    assert queue.queue == ["0", "1", "3"]
