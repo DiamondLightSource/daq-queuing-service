7052b4848da9ac7f2fd01e73c850fc4c52f107d2
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-04-13 14:44:33 +0100

    Flesh out worker

--- src/daq_queuing_service/worker.py
+++ src/daq_queuing_service/worker.py
@@ -1,3 +1,5 @@
+import asyncio
+import logging
 import time
 
 from blueapi.client import BlueapiRestClient
@@ -10,6 +12,7 @@ from pydantic import HttpUrl
 from daq_queuing_service.queue import TaskQueue
 from daq_queuing_service.task import Task
 
+LOGGER = logging.getLogger(__name__)
 I15_1_BLUEAPI_URL = "https://i15-1-blueapi.diamond.ac.uk/"
 
 
@@ -20,33 +23,45 @@ class QueueWorker:
     def __init__(
         self, queue: TaskQueue, blueapi_url: HttpUrl, poll_time_s: float = 1.0
     ):
-        self.queue = queue
         self.poll_time_s = poll_time_s
+        self._queue = queue
         self._url = blueapi_url
         self._client = BlueapiRestClient(config=RestConfig(url=blueapi_url))
 
     async def run_loop(self):
         while True:
-            await self.queue.wait_until_task_available()
-            self._wait_until_blueapi_is_idle()
-            next_task = await self.queue.claim_next_task_once_available()
-            await self.send_task_to_blue_api(next_task)
+            await self._queue.wait_until_task_available()
+            await self._wait_until_blueapi_is_idle()
+            next_task = await self._queue.claim_next_task_once_available()
+            await self._send_task_to_blue_api_and_wait_for_completion(next_task)
 
-    def _wait_until_blueapi_is_idle(self):
+    async def _wait_until_blueapi_is_idle(self):
         while self._client.get_state() != WorkerState.IDLE:
-            time.sleep(self.poll_time_s)
+            await asyncio.sleep(self.poll_time_s)
 
-    async def send_task_to_blue_api(self, task: Task, timeout_s: int = 600):
+    async def _send_task_to_blue_api_and_wait_for_completion(
+        self, task: Task, timeout_s: int = 600
+    ):
         task_request = construct_blueapi_task_request(task)
-        response = self._client.create_task(task_request)
-        blueapi_task_id = response.task_id
         try:
+            response = self._client.create_task(task_request)
+            blueapi_task_id = response.task_id
             self._client.update_worker_task(WorkerTask(task_id=blueapi_task_id))
+        except ServiceUnavailableError as e:
+            # Issue with blueapi worker state or connection - should retry task later
+            await self._queue.return_task_to_queue(task)
+            LOGGER.error(e)
+            return
         except Exception as e:
-            # logger.info("")
-            if isinstance(e, ServiceUnavailableError):
-                await self.queue.return_task_to_queue(task)
-                raise e
-            else:
-                await self.queue.complete_task(task, error=str(e))
-                raise e
+            # Validation issue - task will not work if retried so should cancel task
+            await self._queue.complete_task(task, error=str(e))
+            LOGGER.error(e)
+            return
+        await self.wait_for_task_to_complete(blueapi_task_id, timeout_s)
+        await self._queue.complete_task(task)
+
+    async def wait_for_task_to_complete(self, blueapi_task_id: str, timeout_s: int):
+        blueapi_task = self._client.get_active_task()
+        while blueapi_task.task_id == blueapi_task_id:
+            await asyncio.sleep(self.poll_time_s)
+            blueapi_task = self._client.get_active_task()
