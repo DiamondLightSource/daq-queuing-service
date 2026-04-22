2af210cc7e1b4de6b4f5b4463d5b1e110263d1cc
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-04-22 16:08:10 +0000

    Add script to autogenerate rest api docs

---
+++ utility_scripts/generate_api_docs.py
@@ -0,0 +1,19 @@
+import json
+from unittest.mock import MagicMock
+
+from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+
+from daq_queuing_service.api.api import create_api_router
+
+app = FastAPI()
+app.include_router(create_api_router(MagicMock(), MagicMock(), MagicMock()))
+
+openapi = get_openapi(
+    title=app.title,
+    version=app.version,
+    routes=app.routes,
+)
+
+with open("docs/reference/rest-api.md", "w") as f:
+    f.write(json.dumps(openapi, indent=2))
