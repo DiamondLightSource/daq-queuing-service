a49c68521a6d2ffef6305fe1e26e61d21c192ff9
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-06-22 11:43:52 +0100

    Get udc client ID from env variable

--- src/daq_queuing_service/blueapi_interaction/token_retriever.py
+++ src/daq_queuing_service/blueapi_interaction/token_retriever.py
@@ -8,19 +8,27 @@ from daq_queuing_service.log import LOGGER
 class UDCTokenRetriever:
     """Implements `get_valid_access_token` to get a token using a sealed secret."""
 
-    def __init__(self, client_id: str, secret_variable_name: str = "UDC_SECRET"):
-        self._client_id = client_id
+    def __init__(
+        self,
+        secret_variable_name: str = "UDC_SECRET",
+        client_id_variable_name: str = "UDC_CLIENT_ID",
+    ):
         self._secret_variable_name = secret_variable_name
+        self._client_id_variable_name = client_id_variable_name
 
     def get_valid_access_token(self) -> str:
         token_url = (
             "https://identity.diamond.ac.uk/realms/dls/protocol/openid-connect/token"
         )
 
+        client_id = os.environ.get(self._client_id_variable_name)
         client_secret = os.environ.get(self._secret_variable_name)
 
-        if not (self._client_id and client_secret):
+        if not client_secret:
             LOGGER.debug("No UDC secret found")
+            return ""
+        if not client_id:
+            LOGGER.debug("No UDC client ID found")
             return ""
 
         LOGGER.debug("Found UDC secret")
@@ -28,7 +36,7 @@ class UDCTokenRetriever:
         response = requests.post(
             token_url,
             data={
-                "client_id": self._client_id,
+                "client_id": client_id,
                 "client_secret": client_secret,
                 "grant_type": "client_credentials",
             },
