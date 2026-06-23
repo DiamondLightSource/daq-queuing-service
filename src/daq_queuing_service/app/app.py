0417d7ba7b250f4ad9f7e027caee2bb7b5d9e801
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-06-23 13:16:59 +0100

    Load converter from config (#50)
    * Load converter from config
    
    * Restructure and add test
    
    * Write docs
    
    * Update docs
    
    * Have full path to converter in config
    
    * Update docs
    
    * Update naming
    
    * Add to docs
    
    * Update generated docs

--- src/daq_queuing_service/app/app.py
+++ src/daq_queuing_service/app/app.py
@@ -13,9 +13,9 @@ from daq_queuing_service.api.errors import register_exception_handlers
 from daq_queuing_service.blueapi_interaction.blueapi_adapter import BlueapiClientAdapter
 from daq_queuing_service.broadcaster import Broadcaster
 from daq_queuing_service.plugins.construct_task_request import (
-    construct_blueapi_call_list,
     construct_blueapi_task_request,
 )
+from daq_queuing_service.plugins.converter_utils import get_converter
 from daq_queuing_service.task_queue.queue import QUEUE_EVENTS, TaskQueue
 from daq_queuing_service.worker.worker import QueueWorker
 
@@ -48,7 +48,12 @@ def create_app(dev: bool = False) -> FastAPI:
             await asyncio.gather(worker_task, return_exceptions=True)
 
     config = load_config()
+
     broadcaster: Broadcaster[QUEUE_EVENTS] = Broadcaster()
+
+    converter_path = config.converter.path
+    converter_name = config.converter.name
+    converter = get_converter(converter_path, converter_name)
 
     app = FastAPI(lifespan=lifespan)
 
@@ -61,7 +66,7 @@ def create_app(dev: bool = False) -> FastAPI:
             allow_headers=["*"],
         )
 
-    app.state.queue = TaskQueue(construct_blueapi_call_list, broadcaster)
+    app.state.queue = TaskQueue(converter, broadcaster)
 
     blueapi_rest_client = BlueapiRestClient(config=config.blueapi.api)
     blueapi_client = BlueapiClient.from_config(config.blueapi)
