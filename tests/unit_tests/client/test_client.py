799950257d9fd7da959efa35d1f8b165112ef524
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-07-07 12:07:01 +0100

    Add a test

--- tests/unit_tests/client/test_client.py
+++ tests/unit_tests/client/test_client.py
@@ -1,4 +1,7 @@
-from unittest.mock import MagicMock
+from typing import Any
+from unittest.mock import MagicMock, _Call, call, patch
+
+import pytest
 
 from daq_queuing_service.api.api import create_api_router
 from daq_queuing_service.client.client import QueueClient
@@ -19,3 +22,42 @@ def test_all_endpoints_have_a_client_function():
     assert not not_in_client, (
         f"Found endpoints not covered by a client function: {not_in_client}"
     )
+
+
+@pytest.mark.parametrize(
+    "url, kwargs, expected_request_call",
+    [
+        (
+            "https://google.com/",
+            {"suffix": "/queue"},
+            call("GET", "https://google.com/queue", json=None, params=None),
+        ),
+        (
+            "http://google.com",
+            {
+                "suffix": "/queue",
+                "method": "POST",
+                "data": {"test": "data"},
+                "params": {"test": "params"},
+            },
+            call(
+                "POST",
+                "http://google.com/queue",
+                json={"test": "data"},
+                params={"test": "params"},
+            ),
+        ),
+    ],
+)
+def test__request_makes_request_with_expected_arguments(
+    url: str, kwargs: dict[str, Any], expected_request_call: _Call
+):
+    client = QueueClient(url=url)
+
+    with patch(
+        "daq_queuing_service.client.client.requests.Session.request"
+    ) as mock_request:
+        client._request(**kwargs)
+
+    mock_request.assert_called_once()
+    mock_request.assert_has_calls([expected_request_call])
