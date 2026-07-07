6709404dbb1be905455917c74014d014508e5e0e
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-07-07 11:37:11 +0100

    Improve example

--- docs/examples/add_tasks_using_client.py
+++ docs/examples/add_tasks_using_client.py
@@ -1,6 +1,7 @@
 from blueapi.service.model import TaskRequest
 
 from daq_queuing_service.client.client import QueueClient
+from daq_queuing_service.task_queue.queue import QueueState
 
 # Example of how to add tasks to the queue using the Python client
 # Need a local queue running on port 8001 for this to work
@@ -8,16 +9,37 @@ from daq_queuing_service.client.client import QueueClient
 
 def main():
     client = QueueClient("http://127.0.0.1:8001")
-    n_tasks = 10
 
+    # Pause the queue
+    client.update_queue_state(QueueState(paused=True))
+
+    # Clear everything in the queue
+    client.cancel_all_tasks()
+
+    # Add 10 sleep plans to the queue
     client.add_tasks_to_queue(
         [
             TaskRequest(
-                name="sleep", instrument_session="cm44163-3", params={"time": 10}
+                name="sleep", instrument_session="cm44163-3", params={"time": 1}
             )
-            for _ in range(n_tasks)
+            for _ in range(10)
         ],
     )
+
+    # Add a longer sleep to the start of the queue
+    client.add_tasks_to_queue(
+        [TaskRequest(name="sleep", instrument_session="cm44163-3", params={"time": 5})],
+        position=0,
+    )
+
+    # Print the current tasks in the queue
+    tasks = client.get_queued_tasks()
+    for task in tasks:
+        assert isinstance(task.experiment, TaskRequest)
+        print(
+            f"Task {task.position}: {task.experiment.name} for "
+            + f"{task.experiment.params['time']} seconds"
+        )
 
 
 if __name__ == "__main__":
