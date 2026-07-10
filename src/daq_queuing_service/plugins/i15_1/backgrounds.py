b400a3197fd7e2a8f80597bc4b896e06c00a7bfa
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-07-10 12:59:53 +0100

    Add missing file

---
+++ src/daq_queuing_service/plugins/i15_1/backgrounds.py
@@ -0,0 +1,23 @@
+from typing import Literal
+
+from pydantic import BaseModel
+
+BACKGROUND = Literal["air", "capillary_1", "capillary_2"]
+
+
+class BackgroundInfo(BaseModel):
+    bg_type: BACKGROUND
+    cobra: bool
+    blower: bool
+
+    def add_tiled_id(self, tiled_id: str) -> "TiledBackground":
+        return TiledBackground(
+            bg_type=self.bg_type,
+            cobra=self.cobra,
+            blower=self.blower,
+            tiled_id=tiled_id,
+        )
+
+
+class TiledBackground(BackgroundInfo):
+    tiled_id: str
