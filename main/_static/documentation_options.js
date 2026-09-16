07ab065ad35bdeb9d1331fcb7e405dc9019bf78c
jacob-williamson <jacob-williamson@users.noreply.github.com>
2026-09-16 13:00:15 +0000

    deploy: dfa4f834b516052d3f0164fab4ebe16e966f6b42

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.17.dev3+gbe1d46f70',
+    VERSION: '0.1.18.dev1+gdfa4f834b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
